94d6164e9ac57b89ecdf63c5cf1c77fefb123557
Michele Orselli <mo@ideato.it>
2017-12-12 18:49:12 +0100

    removes redirect in account credits summary endpoint

--- module/Accounting/src/Accounting/Controller/MembersController.php
+++ module/Accounting/src/Accounting/Controller/MembersController.php
@@ -12,6 +12,7 @@ namespace Accounting\Controller;
 use Accounting\Service\AccountService;
 use Application\Controller\OrganizationAwareController;
 use People\Service\OrganizationService;
+use Zend\View\Model\JsonModel;
 
 class MembersController extends OrganizationAwareController
 {
@@ -40,21 +41,72 @@ class MembersController extends OrganizationAwareController
 	 */
 	public function get($id)
 	{
-		if(is_null($this->identity())) {
+		if (is_null($this->identity())) {
 			$this->response->setStatusCode(401);
+
 			return $this->response;
 		}
 
-		$account = $this->accountService->findPersonalAccount($id, $this->organization);
-		if(is_null($account)){
-			$this->response->setStatusCode(404);
-			return $this->response;
-		}
+		$account = $this->accountService
+                        ->findPersonalAccount($id, $this->organization);
 
-		$url = $this->url()->fromRoute('accounts', ['orgId' => $this->organization->getId(), 'id' => $account->getId()]);
-		$this->response->getHeaders()->addHeaderLine('Location', $url);
-		$this->response->setStatusCode(301);
-		return $this->response;
+        if (is_null($account)) {
+            $this->response->setStatusCode(404);
+
+            return $this->response;
+        }
+
+        if (!$this->isAllowed($this->identity(), $account, 'Accounting.Account.get')) {
+            $this->response->setStatusCode(403);
+
+            return $this->response;
+        }
+
+        $endOn = $this->getDateTimeParam('endOn');
+
+        if (is_null($endOn)) {
+            $endOn = new \DateTimeImmutable();
+        }
+
+        $transactions = $this->accountService
+                             ->findTransactions($account, null, null, ['endOn' => $endOn]);
+
+        $totalGeneratedCredits = 0;
+
+        //Date Limits
+        $dateLimitThreeMonths = $endOn->sub(new \DateInterval('P3M'));
+        $dateLimitSixMonths = $endOn->sub(new \DateInterval('P6M'));
+        $dateLimitOneYear = $endOn->sub(new \DateInterval('P1Y'));
+
+        $lastThreeMonthsCredits = 0;
+        $lastSixMonthsCredits = 0;
+        $lastYearCredits = 0;
+
+        foreach ($transactions as $t) {
+            if ($t->getAmount() >= 0) {
+                $totalGeneratedCredits += $t->getAmount();
+
+                if ($t->getCreatedAt() > $dateLimitThreeMonths) {
+                    $lastThreeMonthsCredits += $t->getAmount();//Last 3 Months
+                }
+
+                if ($t->getCreatedAt() > $dateLimitSixMonths) {
+                    $lastSixMonthsCredits += $t->getAmount();//Last 6 Months
+                }
+
+                if ($t->getCreatedAt() > $dateLimitOneYear) {
+                    $lastYearCredits += $t->getAmount();//Last year
+                }
+            }
+        }
+
+        return new JsonModel([
+            'balance' => $account->getBalance()->getValue(),
+            'total'   => $totalGeneratedCredits,
+            'last3M'  => $lastThreeMonthsCredits,
+            'last6M'  => $lastSixMonthsCredits,
+            'last1Y'  => $lastYearCredits
+        ]);
 	}
 
 	/**
