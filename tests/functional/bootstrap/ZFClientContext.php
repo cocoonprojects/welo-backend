696a36da167eea758ec30ab9f135fb8d03203a80
Michele Orselli <mo@ideato.it>
2018-01-04 11:34:38 +0100

    fixes autoload for behat; allows behat to be run in project root

--- tests/functional/bootstrap/ZFClientContext.php
+++ tests/functional/bootstrap/ZFClientContext.php
@@ -41,14 +41,14 @@ class ZFClientContext implements Context
             die();
         }
 
-        echo shell_exec('../vendor/bin/doctrine-module orm:schema-tool:drop --force');
-        echo shell_exec('../vendor/bin/doctrine-module orm:schema-tool:create');
-        echo shell_exec('../vendor/bin/doctrine-module dbal:import ' . __DIR__ . '/../../sql/init.sql');
+        echo shell_exec(__DIR__ . '/../../../vendor/bin/doctrine-module orm:schema-tool:drop --force');
+        echo shell_exec(__DIR__ . '/../../../vendor/bin/doctrine-module orm:schema-tool:create');
+        echo shell_exec(__DIR__ . '/../../../vendor/bin/doctrine-module dbal:import ' . __DIR__ . '/../../sql/init.sql');
     }
 
     /** @AfterSuite */
     public static function teardownApplication(AfterSuiteScope $scope){
-        echo shell_exec('../vendor/bin/doctrine-module orm:schema-tool:drop --force');
+        echo shell_exec(__DIR__ . '/../../../vendor/bin/doctrine-module orm:schema-tool:drop --force');
 
     }
 
