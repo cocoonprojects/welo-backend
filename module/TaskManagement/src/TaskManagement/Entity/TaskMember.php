bc3e87d3af639e7986fd979f3a1af7d29c6ba8e7
Marco Radossi <rad@MBP-di-Marco-4.lan>
2018-01-24 18:06:49 +0100

    fixes wrong (not null) delta when the user skips the shares assignment

--- module/TaskManagement/src/TaskManagement/Entity/TaskMember.php
+++ module/TaskManagement/src/TaskManagement/Entity/TaskMember.php
@@ -198,7 +198,7 @@ class TaskMember
 	public function setShare($value, \DateTime $when) {
 		$this->share = $value;
 		$share = $this->shares->get($this->user->getId()); 
-		$this->delta = is_null($share) ? null : number_format(($share->getValue() - $value), 3);
+		$this->delta = (is_null($share) || is_null($share->getValue())) ? null : number_format(($share->getValue() - $value), 3);
 		$this->mostRecentEditAt = $when;
 		return $this;
 	}
