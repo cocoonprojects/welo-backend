0c2df747a40aca763aa1dd59093abdf26128ce67
Marco Radossi <rad@MBP-di-Marco-4.lan>
2018-03-12 13:46:37 +0100

    cosmetics

--- module/Accounting/test/integration/Accounting/OrganizationStatementsExportTest.php
+++ module/Accounting/test/integration/Accounting/OrganizationStatementsExportTest.php
@@ -72,18 +72,23 @@ class OrganizationStatementsExportTest extends WebTestCase
             $actual[] = str_getcsv($line, ';');
         }
 
-        $this->assertEquals('my org',                   $actual[0][1]);
-        $this->assertEquals('Phil Toledo',              $actual[0][2]);
-        $this->assertEquals('-123,0',                   $actual[0][3]);
-        $this->assertEquals('E anche sti altri due',    $actual[0][4]);
         $this->assertEquals('my org',                   $actual[1][1]);
         $this->assertEquals('Phil Toledo',              $actual[1][2]);
-        $this->assertEquals('-150,0',                   $actual[1][3]);
-        $this->assertEquals('Beccate sti du; crediti',  $actual[1][4]);
-        $this->assertEquals('',                         $actual[2][1]);
-        $this->assertEquals('my org',                   $actual[2][2]);
-        $this->assertEquals('500,0',                    $actual[2][3]);
-        $this->assertEquals('Base deposit',             $actual[2][4]);
+        $this->assertEquals('-123,0',                   $actual[1][3]);
+        $this->assertEquals('227,0',                    $actual[1][4]);
+        $this->assertEquals('E anche sti altri due',    $actual[1][5]);
+
+        $this->assertEquals('my org',                   $actual[2][1]);
+        $this->assertEquals('Phil Toledo',              $actual[2][2]);
+        $this->assertEquals('-150,0',                   $actual[2][3]);
+        $this->assertEquals('350,0',                    $actual[2][4]);
+        $this->assertEquals('Beccate sti du; crediti',  $actual[2][5]);
+
+        $this->assertEquals('',                         $actual[3][1]);
+        $this->assertEquals('my org',                   $actual[3][2]);
+        $this->assertEquals('500,0',                    $actual[3][3]);
+        $this->assertEquals('500,0',                    $actual[3][4]);
+        $this->assertEquals('Base deposit',             $actual[3][5]);
     }
 
 
